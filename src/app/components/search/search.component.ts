0cd18fddc585379729ad251853a9b37bccdf09c9
Laurent Bassin <laurent@bassin.info>
2018-04-30 15:01:49 +0200

    Display containers from ContainerService

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -8,29 +8,12 @@ import {ContainerService} from '../../services/container.service';
 })
 export class SearchComponent implements OnInit {
 
-  containers: Array<any> = [
-    {
-      name: 'Php',
-      image: 'https://cdn.worldvectorlogo.com/logos/php-1.svg'
-    },
-    {
-      name: 'Node.js',
-      image: 'https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png'
-    },
-    {
-      name: 'Ruby',
-      image: 'http://experiences-it.com/wp-content/uploads/2015/09/logoRuby.png'
-    },
-    {
-      name: 'Python',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/2000px-Python-logo-notext.svg.png'
-    }
-  ];
+  containers: Array<any> = [];
 
   constructor(private containerService: ContainerService) {
   }
 
   public ngOnInit(): void {
-    this.containerService.getAvailableContainers().then(response => console.log(response));
+    this.containerService.getAvailableContainers().then(containers => this.containers = containers);
   }
 }
