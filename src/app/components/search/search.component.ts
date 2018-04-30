6d36c02111d2a179a5a700a4511d009120cee3e4
xRaz <maxime.marquet1@gmail.com>
2018-04-30 14:55:29 +0200

    search page finished

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+    containers: Array<any> = [
+        {
+            name: "Php",
+            image: "https://cdn.worldvectorlogo.com/logos/php-1.svg"
+        },
+        {
+            name: "Node.js",
+            image: "https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png"
+        },
+        {
+            name: "Ruby",
+            image: "http://experiences-it.com/wp-content/uploads/2015/09/logoRuby.png"
+        },
+        {
+            name: "Python",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/2000px-Python-logo-notext.svg.png"
+        }
+    ]
 
-  ngOnInit() {
-  }
+    constructor() { }
 
+    ngOnInit() { }
 }
