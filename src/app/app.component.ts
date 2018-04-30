f474ff951522c96ce7b5ed20bd8593e21fe8d4fe
Laurent Bassin <laurent@bassin.info>
2018-04-30 12:32:31 +0200

    WIP - Download yaml config through API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private http: HttpClient) {
+  }
+
+  public ngOnInit(): void {
+    const BASE_URL = 'https://api.github.com';
+    const filePath = 'containers/nginx.yaml';
+
+    const url = BASE_URL + '/repos/lbassin/dockerize-config/contents/' + filePath;
+
+    this.http.get(url).toPromise().then((file: { content: string }) => {
+      const content = atob(file.content);
+
+      console.log(content);
+    });
+
+    console.log('test');
+  }
 }
