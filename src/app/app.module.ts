508ad8cc11f425d4ff4be747cba8bea03774a277
Laurent Bassin <laurent@bassin.info>
2018-05-01 11:20:25 +0200

    Fix code indentation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule} from '@angular/router';
 import {NgModule} from '@angular/core';
+
 // Components
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {HomeComponent} from './components/home/home.component';
 import {SearchComponent} from './components/search/search.component';
 import {SetupComponent} from './components/setup/setup.component';
+
 // Providers
 import {ContainerService} from './services/container.service';
 import {ContainerResolve} from './services/container.resolve';
+
 // Routes
 import {routes} from './app.routes';
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
     component: SetupComponent,
     resolve: {
       container: ContainerResolve,
-    }
+    },
   },
   {
     path: 'search',
