83c549d3401b406629a32b0c50b8d0b41c03d142
Laurent Bassin <laurent@bassin.info>
2018-05-02 11:06:10 +0200

    Move sidebar on all pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import {HttpClientModule} from '@angular/common/http';
 import {HomeComponent} from './components/home/home.component';
 import {SearchComponent} from './components/search/search.component';
 import {SetupComponent} from './components/setup/setup.component';
-import {SidebarComponent} from './components/setup/sidebar/sidebar.component';
+import {SidebarComponent} from './components/sidebar/sidebar.component';
 
 // Providers
 import {ContainerService} from './services/container.service';
