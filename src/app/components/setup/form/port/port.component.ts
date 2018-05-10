b0d842f977510e38e16fba2c1e792798d8880725
= <=>
2018-05-10 18:58:14 +0200

    creation of custom select dropdown

--- src/app/components/setup/form/port/port.component.ts
+++ src/app/components/setup/form/port/port.component.ts
@@ -1,9 +1,37 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   templateUrl: './port.component.html',
   styleUrls: ['./port.component.scss']
 })
 export class PortComponent {
+  @Input() values: Array<String>;
+  possibles: Array<String>;
+  selected: String;
+  show: Boolean = false;
+  constructor() {
+    this.possibles = [
+      '80',
+      '8080',
+      '5050',
+      '6060',
+      '9999',
+      '666'
+    ];
+  }
 
+  changeSelected(value: any) {
+    console.log(value);
+    this.selected = value;
+  }
+
+  showPossibles() {
+    console.log('showPossibles');
+    this.show = true;
+  }
+
+  hidePossibles() {
+    console.log('hidePossibles');
+    this.show = false;
+  }
 }
