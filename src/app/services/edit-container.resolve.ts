aeb78fc148eb045b4c9ce6f1e8b3feb7941dbf76
Laurent Bassin <laurent@bassin.info>
2018-05-11 19:58:55 +0200

    Add edit link in sidebar

--- src/app/services/edit-container.resolve.ts
+++ src/app/services/edit-container.resolve.ts
@@ -1,26 +1,15 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, Resolve} from '@angular/router';
 import {Container} from '../models/container.model';
-import {ContainerService} from './container.service';
 import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class EditContainerResolve implements Resolve<Container> {
 
-  constructor(private containerService: ContainerService) {
-  }
-
   resolve(route: ActivatedRouteSnapshot): Observable<Container> {
     return new Observable<Container>(observer => {
-
-      this.containerService.getContainerConfig(route.params.id)
-        .then(container => {
-          observer.next(container);
-          observer.complete();
-        })
-        .catch(() => {
-          observer.complete();
-        });
+      observer.next(null);
+      observer.complete();
     });
   }
 
