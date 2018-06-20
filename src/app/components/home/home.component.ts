32f3a33cf78bc8aa735a7d063dd8a39b242b6d63
Laurent Bassin <laurent@bassin.info>
2018-06-20 12:59:58 +0200

    #73 Hide sidebar on home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SidebarService } from '../../services/sidebar.service';
 import { CustomTitleService } from '../../services/title.service';
+import { ProjectService } from '../../services/project.service';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
     redirection = false;
     sidebarVisible = false;
@@ -16,13 +17,21 @@ export class HomeComponent implements OnInit {
     constructor(
         private router: Router,
         private sidebarService: SidebarService,
-        private titleService: CustomTitleService
+        private titleService: CustomTitleService,
+        private projectService: ProjectService,
     ) {
         this.sidebarService.getObservable().subscribe(value => this.sidebarVisible = value);
     }
 
     ngOnInit(): void {
         this.titleService.resetTitle();
+        this.sidebarService.hide();
+    }
+
+    ngOnDestroy(): void {
+        if (this.projectService.getContainers().length) {
+            this.sidebarService.show();
+        }
     }
 
     goToSearch() {
