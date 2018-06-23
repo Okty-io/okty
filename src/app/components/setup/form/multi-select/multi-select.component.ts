b41edf448e99678a616aec3827cb730cc8d3f59c
Laurent Bassin <laurent@bassin.info>
2018-06-23 15:38:34 +0200

    Fix multi-select separator

--- src/app/components/setup/form/multi-select/multi-select.component.ts
+++ src/app/components/setup/form/multi-select/multi-select.component.ts
@@ -2,112 +2,112 @@ import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/cor
 import { FormControl } from '@angular/forms';
 
 @Component({
-	templateUrl: './multi-select.component.html',
-	styleUrls: ['./multi-select.component.scss']
+    templateUrl: './multi-select.component.html',
+    styleUrls: ['./multi-select.component.scss']
 })
 export class MultiSelectComponent implements OnInit, AfterViewInit {
-	@Input() formControl: FormControl;
-	@Input() input: any;
-	@ViewChild('selectizeInput') selectizeInput;
+    @Input() formControl: FormControl;
+    @Input() input: any;
+    @ViewChild('selectizeInput') selectizeInput;
 
-	private highestLength = 50;
-	public selected: Array<String> = [];
-	public selections: Array<{ label: string, value: string }> = [];
-	public showPossibles = false;
+    private highestLength = 50;
+    public selected: Array<String> = [];
+    public selections: Array<{ label: string, value: string }> = [];
+    public showPossibles = false;
 
-	ngOnInit(): void {
-		this.initSelectedValues();
-		this.refineSelections();
-	}
+    ngOnInit(): void {
+        this.initSelectedValues();
+        this.refineSelections();
+    }
 
-	ngAfterViewInit() {
-		this.selectizeInput.nativeElement.style.width = ((this.selectizeInput.nativeElement.value.length + 1) * 6.8) + 'px';
-	}
+    ngAfterViewInit() {
+        this.selectizeInput.nativeElement.style.width = ((this.selectizeInput.nativeElement.value.length + 1) * 6.8) + 'px';
+    }
 
-	initSelectedValues(): void {
-		if (!this.formControl.value) {
-			return;
-		}
+    initSelectedValues(): void {
+        if (!this.formControl.value) {
+            return;
+        }
 
-		this.formControl.value.toString().split(';').forEach((selected) => {
-			this.addSelected(selected);
-		});
-	}
+        this.formControl.value.toString().split(',').forEach((selected) => {
+            this.addSelected(selected);
+        });
+    }
 
 
-	addSelected(value: string) {
-		this.selected.push(value);
-		this.selectizeInput.nativeElement.value = '';
-		this.refineSelections();
-		this.updateFormControlValue();
-	}
+    addSelected(value: string) {
+        this.selected.push(value);
+        this.selectizeInput.nativeElement.value = '';
+        this.refineSelections();
+        this.updateFormControlValue();
+    }
 
-	checkInput(event: Event): void {
-		const target = <HTMLInputElement>event.target;
-		this.resizeInput(target);
-		this.refineSelections(target.value);
-	}
+    checkInput(event: Event): void {
+        const target = <HTMLInputElement>event.target;
+        this.resizeInput(target);
+        this.refineSelections(target.value);
+    }
 
-	focusTheInput(target: HTMLElement): void {
-		if (!target.classList.contains('selectize-selected')) {
-			this.selectizeInput.nativeElement.focus();
-		}
-	}
+    focusTheInput(target: HTMLElement): void {
+        if (!target.classList.contains('selectize-selected')) {
+            this.selectizeInput.nativeElement.focus();
+        }
+    }
 
-	hidePossiblesContainer(): void {
-		setTimeout(() => {
-			this.showPossibles = false;
-			this.selectizeInput.nativeElement.value = '';
-			this.refineSelections();
-		}, 200);
-	}
+    hidePossiblesContainer(): void {
+        setTimeout(() => {
+            this.showPossibles = false;
+            this.selectizeInput.nativeElement.value = '';
+            this.refineSelections();
+        }, 200);
+    }
 
-	refineSelections(value: string = ''): void {
-		this.selections = [];
-		for (const data in this.input.source) {
-			if (!this.input.source.hasOwnProperty(data) || this.selected.includes(data)) {
-				continue;
-			}
+    refineSelections(value: string = ''): void {
+        this.selections = [];
+        for (const data in this.input.source) {
+            if (!this.input.source.hasOwnProperty(data) || this.selected.includes(data)) {
+                continue;
+            }
 
-			if (value !== '' && data.indexOf(value) === -1) {
-				continue;
-			}
+            if (value !== '' && data.indexOf(value) === -1) {
+                continue;
+            }
 
-			this.selections.push({
-				label: this.input.source[data],
-				value: data,
-			});
-		}
-	}
+            this.selections.push({
+                label: this.input.source[data],
+                value: data,
+            });
+        }
+    }
 
-	removeSelected(value: string): void {
-		this.selected.splice(this.selected.indexOf(value), 1);
-		this.refineSelections();
-		this.updateFormControlValue();
-	}
+    removeSelected(value: string): void {
+        this.selected.splice(this.selected.indexOf(value), 1);
+        this.refineSelections();
+        this.updateFormControlValue();
+    }
 
-	updateFormControlValue(): void {
-		let formControlValue = '';
-		this.selected.forEach((data: string) => {
-			formControlValue += (formControlValue.length > 0 ? ';' : '') + data;
-		});
+    updateFormControlValue(): void {
+        let formControlValue = '';
+        this.selected.forEach((data: string) => {
+            formControlValue += (formControlValue.length > 0 ? ',' : '') + data;
+        });
 
-		this.formControl.setValue(formControlValue);
-	}
+        this.formControl.setValue(formControlValue);
+    }
 
-	resizeInput(target: HTMLInputElement): void {
-		if (target.value.length > 11) {
-			target.value = target.value.substr(0, this.highestLength);
-		}
+    resizeInput(target: HTMLInputElement): void {
+        if (target.value.length > 11) {
+            target.value = target.value.substr(0, this.highestLength);
+        }
 
-		if (target.value.length > 0) {
-			target.style.width = ((target.value.length + 1) * 6.9) + 'px';
-		} else {
-			target.style.width = '';
-		}
-	}
+        if (target.value.length > 0) {
+            target.style.width = ((target.value.length + 1) * 6.9) + 'px';
+        } else {
+            target.style.width = '';
+        }
+    }
 
-	showPossiblesContainer(): void {
-		this.showPossibles = true;
-	}
+    showPossiblesContainer(): void {
+        this.showPossibles = true;
+    }
 }
