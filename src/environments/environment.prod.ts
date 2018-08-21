73a72132168b6e47ce937f2cbdaf810b33e1afc3
Laurent Bassin <laurent@bassin.info>
2018-08-21 22:31:56 +0200

    Display version from package.json and update package.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
+// @ts-ignore
+import * as Package from '../../package.json';
 import { GithubService } from '../app/services/config/github.service';
 
 export const environment = {
   production: true,
-  configService: GithubService
+  configService: GithubService,
+  version: Package.version
 };
