edf470ae978fed7ecbff626822db2bb8e1b04877
Laurent Bassin <laurent@bassin.info>
2018-11-04 20:08:10 +0100

    Add loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { SidebarService } from './services/sidebar.service';
 import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { NavigationEnd, Router } from '@angular/router';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterEvent } from '@angular/router';
 import { environment } from '../environments/environment';
 
 @Component({
@@ -16,6 +16,8 @@ export class AppComponent implements OnInit, OnDestroy {
   private routerSubscription: Subscription;
 
   public version = environment.version;
+
+  loading = false;
 
   constructor(private sidebarStatus: SidebarService,
               private changeDetector: ChangeDetectorRef,
@@ -35,15 +37,34 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   private initGoogleAnalytics(): void {
-    if (!environment.production || !window.localStorage) {
-      return;
-    }
+    this.routerSubscription = this.router.events.subscribe((event: RouterEvent) => {
+      this.navigationInterceptor(event);
 
-    this.routerSubscription = this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
+        if (!environment.production || !window.localStorage) {
+          return;
+        }
+
         this.updateGoogleAnalyticsPage(event.urlAfterRedirects);
       }
     });
+  }
+
+  private navigationInterceptor(event: RouterEvent): void {
+    if (event instanceof NavigationStart) {
+      this.loading = true;
+    }
+    if (event instanceof NavigationEnd) {
+      this.loading = false;
+    }
+
+    // Set loading state to false in both of the below events to hide the spinner in case a request fails
+    if (event instanceof NavigationCancel) {
+      this.loading = false;
+    }
+    if (event instanceof NavigationError) {
+      this.loading = false;
+    }
   }
 
   private updateGoogleAnalyticsPage(url: string): void {
