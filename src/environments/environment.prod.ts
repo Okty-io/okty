39b348018b4cf9d2a38e01e6c9a554368c1a7ca6
Laurent Bassin <laurent@bassin.info>
2018-11-13 17:07:37 +0100

    Remove old sources

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  api: {
-    host: 'https://api.okty.io'
-  },
-  version: require('../../package.json').version
-};
--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  api: {
-    host: 'http://127.0.0.1:8080'
-  },
-  version: require('../../package.json').version
-};
