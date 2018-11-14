edf68fc496e939398b34d510bc2302b72b6c3c1a
w-raken <maxime.marquet1@gmail.com>
2018-11-14 15:23:38 +0100

    Refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { InlineSVGModule } from 'ng-inline-svg';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {InlineSVGModule} from 'ng-inline-svg';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {HomeComponent} from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    InlineSVGModule.forRoot()
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        InlineSVGModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import anime from 'animejs'
+import {Component, OnInit} from '@angular/core';
+import * as anime from 'animejs';
 
 @Component({
     selector: 'app-home',
@@ -9,47 +9,48 @@ import anime from 'animejs'
 export class HomeComponent implements OnInit {
 
     private relativeOffset = anime.timeline({
-        //direction: 'alternate',
         loop: true
     });
 
-    constructor() { }
+    constructor() {
+    }
 
-    ngOnInit() { }
+    ngOnInit() {
+    }
 
     private animeIso(): void {
         this.relativeOffset
-        .add({
-            targets: 'svg .cls-1 #Calque_2 #Calque_12 .gr1',
-            strokeDashoffset: [anime.setDashoffset, 0],
-            easing: 'easeInQuint',
-            duration: 1000
-        })
-        .add({
-            targets: 'svg .cls-1 #Calque_2 #Calque_12 #line4',
-            strokeDashoffset: [anime.setDashoffset, 0],
-            easing: 'easeOutExpo',
-            duration: 1000
-        })
-        .add({
-            targets: 'svg .cls-1 #Calque_2 #Calque_12 #line5',
-            strokeDashoffset: [anime.setDashoffset, 0],
-            easing: 'easeOutExpo',
-            duration: 1000,
-            offset: '-=1000'
-        })
-        .add({
-            targets: 'svg .cls-1 #Calque_2 #Calque_12 #line6',
-            strokeDashoffset: [anime.setDashoffset, 0],
-            easing: 'easeOutExpo',
-            duration: 1000,
-            offset: '-=500'
-        })
-        .add({
-            targets: 'svg .cls-1 #Calque_2 #Calque_12',
-            easing: 'easeOutExpo',
-            duration: 800,
-            opacity: 0
-        });
+            .add({
+                targets: 'svg .cls-1 #Calque_2 #Calque_12 .gr1',
+                strokeDashoffset: [anime.setDashoffset, 0],
+                easing: 'easeInQuint',
+                duration: 1000
+            })
+            .add({
+                targets: 'svg .cls-1 #Calque_2 #Calque_12 #line4',
+                strokeDashoffset: [anime.setDashoffset, 0],
+                easing: 'easeOutExpo',
+                duration: 1000
+            })
+            .add({
+                targets: 'svg .cls-1 #Calque_2 #Calque_12 #line5',
+                strokeDashoffset: [anime.setDashoffset, 0],
+                easing: 'easeOutExpo',
+                duration: 1000,
+                offset: '-=1000'
+            })
+            .add({
+                targets: 'svg .cls-1 #Calque_2 #Calque_12 #line6',
+                strokeDashoffset: [anime.setDashoffset, 0],
+                easing: 'easeOutExpo',
+                duration: 1000,
+                offset: '-=500'
+            })
+            .add({
+                targets: 'svg .cls-1 #Calque_2 #Calque_12',
+                easing: 'easeOutExpo',
+                duration: 800,
+                opacity: 0
+            });
     }
 }
