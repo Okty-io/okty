98ab5189f0205d5b271057ebe3e23480d91f8864
Laurent Bassin <laurent@bassin.info>
2018-11-14 17:03:46 +0100

    Change project structure

---
+++ src/app/modules/generator/generator.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GeneratorRoutingModule } from './generator-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GeneratorRoutingModule
+  ]
+})
+export class GeneratorModule { }
---
+++ src/app/modules/user/user.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserRoutingModule } from './user-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    UserRoutingModule
+  ]
+})
+export class UserModule { }
