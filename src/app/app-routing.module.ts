02bd99408f9694d01e94a3beb0b12abb70ad7c27
w-raken <maxime.marquet1@gmail.com>
2018-11-14 16:26:00 +0100

    Add search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from './home/home.component';
+import {HomeComponent} from './components/home/home.component';
 
 const routes: Routes = [
     {
