86405f53f0dda034c113d33ec8261fb30d830f19
Laurent Bassin <laurent@bassin.info>
2018-11-15 12:29:01 +0100

    WIP - Animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ContainersResolver } from './core/resolvers/containers.resolver';
 import { TemplatesResolver } from './core/resolvers/templates.resolver';
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [
@@ -14,6 +15,7 @@ import { AppRoutingModule } from './app-routing.module';
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         AppRoutingModule,
         CoreModule
     ],
