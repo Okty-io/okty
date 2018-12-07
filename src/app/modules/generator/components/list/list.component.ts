602647c56dd4e1e174f36fa9ccd84e6f5feaf022
Laurent Bassin <laurent@bassin.info>
2018-12-07 14:26:34 +0100

    Add list/search components + container model

---
+++ src/app/modules/generator/components/list/list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-generator-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/generator/generator.module.ts
+++ src/app/modules/generator/generator.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { GeneratorRoutingModule } from './generator-routing.module';
 import { ContainersComponent } from './pages/containers/containers.component';
 import { TemplatesComponent } from './pages/templates/templates.component';
+import { SearchComponent } from './components/search/search.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
-  declarations: [ContainersComponent, TemplatesComponent],
+  declarations: [ContainersComponent, TemplatesComponent, SearchComponent, ListComponent],
   imports: [
     CommonModule,
     GeneratorRoutingModule
