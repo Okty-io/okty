6050e96683009efd4287da75998897a17e4d8d9a
Laurent Bassin <laurent@bassin.info>
2018-12-09 11:54:38 +0100

    Add search on containers

--- src/app/modules/generator/components/list/list.component.ts
+++ src/app/modules/generator/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import Listable from '../../../../core/interfaces/listable';
 
 @Component({
@@ -6,15 +6,10 @@ import Listable from '../../../../core/interfaces/listable';
     templateUrl: './list.component.html',
     styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit {
+export class ListComponent {
 
     @Input() elements: Array<Listable>;
 
     constructor() {
     }
-
-    ngOnInit() {
-        console.log(this.elements);
-    }
-
 }
