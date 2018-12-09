d7ede6ebd2a96b3ac461528b7fe5e38de37d0e09
Laurent Bassin <laurent@bassin.info>
2018-12-09 11:00:19 +0100

    Handle error when fetching containers

--- src/app/modules/generator/generator-routing.module.ts
+++ src/app/modules/generator/generator-routing.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContainersComponent } from './pages/containers/containers.component';
 import { TemplatesComponent } from './pages/templates/templates.component';
-import { ContainersResolver } from '../../core/resolvers/containers.resolver';
 
 const routes: Routes = [
     {
         path: 'containers',
-        component: ContainersComponent,
-        resolve: {
-            containers: ContainersResolver
-        }
+        component: ContainersComponent
     },
     {
         path: 'templates',
