ce57a88e4bcd84fb12c9a96854be7b0db68d3576
Laurent Bassin <laurent@bassin.info>
2019-02-24 22:42:00 +0100

    Gitlab auth

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -5,5 +5,8 @@ export const environment = {
     },
     github: {
         clientId: 'fecacceedbe16b50bf3e'
+    },
+    gitlab: {
+        clientId: 'ec52e389d991ddbf48c4bd1d58ac64a60ea6e67f13657ff7848a8eddc27757f5'
     }
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,8 @@ export const environment = {
     },
     github: {
         clientId: '7397a85d56421ef8af97'
+    },
+    gitlab: {
+        clientId: 'b8747a953ff6311d20ab48fd8e91e86ec457d75200878601ecfaf15a4f743f4b'
     }
 };
