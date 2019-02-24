f67154eba5dcd618964f0e45bd998f48cf2bc052
Laurent Bassin <laurent@bassin.info>
2019-02-24 13:37:16 +0100

    Remove JWT token if expired

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -2,18 +2,26 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable()
 export class ApiService {
 
+    private jwtHelper: JwtHelperService;
     private readonly baseUrl: string;
 
     constructor(private http: HttpClient) {
         this.baseUrl = environment.api.host;
+        this.jwtHelper = new JwtHelperService();
     }
 
-    private static getHeaders(): HttpHeaders {
-        const token = localStorage.getItem('token');
+    private getHeaders(): HttpHeaders {
+        let token = localStorage.getItem('token');
+        if (this.jwtHelper.isTokenExpired(token)) {
+            localStorage.removeItem('token');
+            token = null;
+        }
+
         if (!token) {
             return new HttpHeaders();
         }
@@ -24,19 +32,19 @@ export class ApiService {
     }
 
     public get(endpoint: string): Observable<any> {
-        const headers = ApiService.getHeaders();
+        const headers = this.getHeaders();
 
         return this.http.get(`${this.baseUrl}/${endpoint}`, {headers: headers});
     }
 
     public post(endpoint: string, data: any): Observable<any> {
-        const headers = ApiService.getHeaders();
+        const headers = this.getHeaders();
 
         return this.http.post(`${this.baseUrl}/${endpoint}`, data, {responseType: 'json', headers: headers});
     }
 
     public download(endpoint: string, data: any): Observable<Blob> {
-        const headers = ApiService.getHeaders();
+        const headers = this.getHeaders();
 
         return this.http.post(`${this.baseUrl}/${endpoint}`, data, {responseType: 'blob', headers: headers});
     }
