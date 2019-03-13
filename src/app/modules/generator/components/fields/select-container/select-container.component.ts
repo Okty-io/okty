1149c4ed21eae25d70ff311ad164996586b753fc
Laurent Bassin <laurent@bassin.info>
2019-03-13 19:07:29 +0100

    Add container select

--- src/app/modules/generator/components/fields/select-container/select-container.component.ts
+++ src/app/modules/generator/components/fields/select-container/select-container.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ContainerConfigField } from '../../../models/container';
+import { SessionService } from '../../../services/session.service';
+import { ContainerFormData } from '../../../interfaces/form-data';
+import { ContainerService } from '../../../services/container.service';
 
 @Component({
     templateUrl: './select-container.component.html',
@@ -13,7 +16,27 @@ export class SelectContainerComponent implements OnInit {
 
     options: Array<{ label: string; value: string }>;
 
+    constructor(private session: SessionService, private containerService: ContainerService) {
+    }
+
     ngOnInit(): void {
         this.options = [];
+
+        this.session.getContainers().forEach((data: ContainerFormData) => {
+            const container = this.containerService.formDataToApiArg(data);
+
+            this.options.push({
+                label: container.args.id,
+                value: container.args.id
+            });
+        });
+
+        const containerExists = this.options.find((option) => {
+            return option.value === this.formControl.value;
+        });
+
+        if (this.formControl.value.trim().length === 0 || !containerExists) {
+            this.formControl.setValue(null);
+        }
     }
 }
