362a3347f551665e0f42d938f687bace0175dd2a
Laurent Bassin <laurent@bassin.info>
2019-03-20 22:13:59 +0100

    Add CMS Module

--- src/app/modules/cms/cms-routing.module.ts
+++ src/app/modules/cms/cms-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
+    {
+        path: 'privacy',
+        component: PrivacyComponent
+    },
     {
         path: '',
         component: HomeComponent,
@@ -10,10 +15,9 @@ const routes: Routes = [
     }
 ];
 
-
 @NgModule({
     imports: [RouterModule.forChild(routes)],
     exports: [RouterModule]
 })
-export class HomeRoutingModule {
+export class CmsRoutingModule {
 }
--- src/app/modules/cms/cms.module.ts
+++ src/app/modules/cms/cms.module.ts
@@ -1,23 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HomeRoutingModule } from './home-routing.module';
-import { InlineSVGModule } from 'ng-inline-svg';
-import { HttpClientModule } from '@angular/common/http';
+import { CmsRoutingModule } from './cms-routing.module';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PageIllustrationComponent } from './components/page-illustration/page-illustration.component';
+import { InlineSVGModule } from 'ng-inline-svg';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     declarations: [
         HomeComponent,
+        PrivacyComponent,
         PageIllustrationComponent
     ],
     imports: [
         CommonModule,
-        HomeRoutingModule,
+        CmsRoutingModule,
         HttpClientModule,
         InlineSVGModule.forRoot()
     ]
 })
-export class HomeModule {
+export class CmsModule {
 }
