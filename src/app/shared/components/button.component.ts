a758e61b7db517ac6120484995ebb5a67c3ea383
Laurent Bassin <laurent@bassin.info>
2019-03-24 16:52:11 +0100

    Remove unused code + Clean

--- src/app/shared/components/button.component.ts
+++ src/app/shared/components/button.component.ts
@@ -7,16 +7,7 @@ import { Component, Input } from '@angular/core';
             <ng-content></ng-content>
         </button>
     `,
-    styles: [
-        'button { border: none; font-family: \'Open Sans\', sans-serif; cursor: pointer; }',
-        '.primary { background: #3D68DD; color: #FFF }',
-        '.light { background: #FFF; color: #000; border: 1px solid rgba(120, 120, 120, 0.5); }',
-        '.danger { background: #D80303; color: #FFF;}',
-        '.small { }',
-        '.medium { padding: 10px 25px; border-radius: 30px; }',
-        '.large { }',
-        '[disabled] { opacity: 0.4; }'
-    ]
+    styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent {
 
