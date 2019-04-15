013747b3e66741d49570edc39c72f4e86491a4bf
Laurent Bassin <laurent@bassin.info>
2019-04-15 16:27:26 +0200

    Link between learning and chapters

--- src/app/modules/learning/learning.module.ts
+++ src/app/modules/learning/learning.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { LearningRoutingModule } from './learning-routing.module';
 import { IndexComponent } from './pages/index/index.component';
 import { ChaptersComponent } from './pages/chapters/chapters.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
     declarations: [IndexComponent, ChaptersComponent],
     imports: [
         CommonModule,
-        LearningRoutingModule
+        LearningRoutingModule,
+        SharedModule,
     ]
 })
 export class LearningModule {
